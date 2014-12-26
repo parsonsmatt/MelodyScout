bdbb05af3b2b333ed90f39b22e3173cf21735ec2
Matt Parsons <parsonsmatt@gmail.com>
2014-12-26 01:31:25 -0500

    Add has_secure_password to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, 
                     length: { maximum: 255 }, 
                     uniqueness: { case_sensitive: false }
+
+  has_secure_password
 end
