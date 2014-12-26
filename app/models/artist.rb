6a8569fb9f000b78e4927ade82e273644742b022
Matt Parsons <parsonsmatt@gmail.com>
2014-12-26 13:01:36 -0500

    Add Memberships relationship to Artist model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,9 @@
 class Artist < ActiveRecord::Base
+  # Members:
+  has_many :memberships
+  has_many :artists, through: :memberships
+
+  # Releases:
   has_many :contributions
   has_many :releases, through: :contributions
 end
