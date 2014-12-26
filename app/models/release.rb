326c320df1a66f8b5921b7d8c76ac834f8d49df3
Matt Parsons <parsonsmatt@gmail.com>
2014-12-26 11:01:36 -0500

    Verify release has release date

--- app/models/release.rb
+++ app/models/release.rb
@@ -1,4 +1,6 @@
 class Release < ActiveRecord::Base
+  validates :date, presence: true
+
   has_many :contributions
   has_many :artists, through: :contributions
 end
--- test/models/release_test.rb
+++ test/models/release_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ReleaseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @release = Release.new(name: "Test Release", description: "Lorem Ipsum", date: 1.year.ago)
+    @release.artists.new(name: "Stub artist")
+  end
+
+  test "should have release date" do
+    @release.date = nil
+    assert_not @release.valid?
+  end
 end
