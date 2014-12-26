989c47c98b0d454979796dc91fa9c6a044b70f4e
Matt Parsons <parsonsmatt@gmail.com>
2014-12-26 01:05:21 -0500

    Add uniqueness to User email in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
-  validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }
+  validates :name, presence: true, 
+                   length: { maximum: 50 }
+  validates :email, presence: true, 
+                    length: { maximum: 255 }, 
+                    uniqueness: { case_sensitive: false }
 end
