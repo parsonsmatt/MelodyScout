80a475666b370a7353450dccb385a2a95cbda34d
Matt Parsons <parsonsmatt@gmail.com>
2014-12-26 21:53:31 -0500

    Remove all membership/band information

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,14 +1,4 @@
 class Artist < ActiveRecord::Base
-  # Members:
-  has_many :members, class_name: "Membership",
-                     foreign_key: "member_id",
-                     dependent: :destroy
-
-  # Bands:
-  has_many :bands, class_name: "Membership",
-                   foreign_key: "band_id",
-                   dependent: :destroy
-
   # Releases:
   has_many :contributions
   has_many :releases, through: :contributions
