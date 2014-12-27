d8d30e8a09c0887346d49f56ee4583d342e593a0
Matt Parsons <parsonsmatt@gmail.com>
2014-12-26 22:30:48 -0500

    writing tests first

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,14 @@
 class Artist < ActiveRecord::Base
+  
+  validates :name, presence: true
   # Releases:
   has_many :contributions
   has_many :releases, through: :contributions
+
+  # Members:
+  has_many :memberships, class_name: "Membership", foreign_key: "band_id", dependent: :destroy
+
+  # Bands:
+  has_many :bandships, class_name: "Membership", foreign_key: "member_id", dependent: :destroy
+
 end
--- test/models/artist_test.rb
+++ test/models/artist_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class ArtistTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @artist = Artist.new(name: "test artist")
+  end
+
+  test "invalid without name" do
+    @artist.name = ""
+    assert_not @artist.valid?
+  end
+
+  test "can have members" do
+    @artist.add_member(Artist.new(name: "2nd test"))
+    assert @artist.members.count == 1
+  end
 end
