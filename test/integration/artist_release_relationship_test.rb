1f00ab6ea48a816a0c009907629db2008e993f32
Matt Parsons <parsonsmatt@gmail.com>
2014-12-27 16:24:21 -0500

    artst-release relationship tested

--- test/integration/artist_release_relationship_test.rb
+++ test/integration/artist_release_relationship_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class ArtistReleaseRelationshipTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :artists, :releases
+
+  def setup 
+    @a1 = artists(:one)
+    @r1 = releases(:one)
+  end
+
+  test "can add a release to artist" do
+    count = @a1.releases.count
+    assert @a1.add_release(@r1)
+    assert_equal count+1, @a1.releases.count
+  end
+
+  test "can add an artist to release" do
+    count = @r1.artists.count
+    assert @r1.add_artist(@a1)
+    assert_equal count+1, @r1.artists.count
+  end
+
+  test "can check if artist includes release" do
+    assert_not @a1.releases.include?(@r1)
+  end
+
+  test "can't duplicate relationship" do
+    @a1.add_release(@r1)
+    assert_raises(ActiveRecord::RecordNotUnique) { @a1.add_release(@r1) }
+  end
+
+  test "can't duplicate from asymmetry" do
+    @a1.add_release(@r1)
+    assert_raises(ActiveRecord::RecordNotUnique) { @r1.add_artist(@a1) }
+  end
 end
