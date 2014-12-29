ae26ae8ef5d4720cdddcbcca3110039c670bf5a7
Matt Parsons <parsonsmatt@gmail.com>
2014-12-29 18:10:16 -0500

    Memberships index on Artist

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,5 +1,15 @@
 class MembershipsController < ApplicationController
+  before_action :get_artist, only: [:index]
 
+  # GET /:artist/:artist_id/memerberships
   def index
+    @members = @artist.members
+    @bands = @artist.bands
   end
+
+  private
+
+    def get_artist
+      @artist = Artist.find_by(id: params[:artist_id])
+    end
 end
