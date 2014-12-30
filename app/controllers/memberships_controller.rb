4cc1a4df9508f5caa92667d188545e6b5f73117f
Matt Parsons <parsonsmatt@gmail.com>
2014-12-29 21:13:43 -0500

    Form to add member shows

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,6 +1,11 @@
 class MembershipsController < ApplicationController
   before_action :get_artist, only: [:index, :new]
 
+  def new
+    symbol = @artist.individual? ? :member_id : :band_id;
+    @membership = Membership.new( symbol => @artist.id)
+  end
+
   private
 
     def get_artist
