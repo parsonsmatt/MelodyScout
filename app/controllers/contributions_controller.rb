364441302e0b7762c7095e6a0196204c2d276384
Matt Parsons <parsonsmatt@gmail.com>
2014-12-30 01:04:22 -0500

    Controller finds artist/release

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,7 +1,10 @@
 class ContributionsController < ApplicationController
   before_action :set_contribution, only: [:show, :edit, :update, :destroy]
-
-  # GET /contributions
+  before_action :set_artist, only: [:new, :create]
+  before_action :set_release, only: [:new, :create]
+  
+  # GET /artists/:artist_id/contributions
+  # GET /releases/:release_id/contributions
   def index
     @contributions = Contribution.all
   end
@@ -10,10 +13,14 @@ class ContributionsController < ApplicationController
   def show
   end
 
+  # GET /artists/:artist_id/contributions/new
+  # GET /releases/:release_id/contributions/new
   def new
     @contribution = Contribution.new
   end
 
+  # POST /artists/:artist_id/contributions
+  # POST /releases/:release_id/contributions
   def create
     @contribution = Contribution.new(contribution_params)
     
@@ -33,11 +40,19 @@ class ContributionsController < ApplicationController
 
   private
   
-  def set_contribution
-    @contribution = Contribution.find(params[:id])
-  end
+    def set_contribution
+      @contribution = Contribution.find(params[:id])
+    end
 
-  def contribution_params
-    params.require(:contribution).permit(:artist,:release,:artist_id)
-  end
+    def set_artist
+      @artist = Artist.find_by(id: params[:artist_id])
+    end
+
+    def set_release
+      @release = Release.find_by(id: params[:release_id])
+    end
+
+    def contribution_params
+      params.require(:contribution).permit(:artist,:release,:artist_id)
+    end
 end
--- test/controllers/contributions_controller_test.rb
+++ test/controllers/contributions_controller_test.rb
@@ -2,7 +2,4 @@ require 'test_helper'
 
 class ContributionsControllerTest < ActionController::TestCase
 
-  test "create contribution" do
-    assert Contribution.new(artist_id: 1, release_id: 1)
-  end
 end
