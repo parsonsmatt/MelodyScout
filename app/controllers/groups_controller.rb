1729c4eb8715b2be50620fb5011c7223a30f5648
Matt Parsons <parsonsmatt@gmail.com>
2014-12-31 14:39:28 -0500

    Add new action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,15 @@
 class GroupsController < ApplicationController
+  before_action :set_member, only: :new
+
+  # GET artists/:artist_id/groups/new
+  def new
+    @membership = Membership.new
+  end
+
+  private
+
+    def set_member
+      @member = Artist.find_by(id: params[:artist_id])
+    end
+
 end
