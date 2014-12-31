cf781ae146fbd67a9f132cad8b680db9a046203b
Matt Parsons <parsonsmatt@gmail.com>
2014-12-31 18:23:21 -0500

    rails g migration AddRoleToMemberships role:string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141230004815) do
+ActiveRecord::Schema.define(version: 20141231232244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20141230004815) do
     t.integer  "member_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "role"
   end
 
   add_index "memberships", ["band_id", "member_id"], name: "index_memberships_on_band_id_and_member_id", unique: true, using: :btree
