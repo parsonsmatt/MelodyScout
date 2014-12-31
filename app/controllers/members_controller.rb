e55878d57b64344331cd3b67100234ca00620924
Matt Parsons <parsonsmatt@gmail.com>
2014-12-31 16:38:57 -0500

    Add new for members controller

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,13 @@
 class MembersController < ApplicationController
+  before_action :set_group, only: [:new]
+
+  # GET /artists/:artist_id/members/new
+  def new
+  end
+
+  private
+
+    def set_group
+      @group = Artist.find(params[:artist_id])
+    end
 end
