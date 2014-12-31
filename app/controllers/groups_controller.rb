6dc10474341a4f632a33a24e3d353e0ead5e55e5
Matt Parsons <parsonsmatt@gmail.com>
2014-12-31 15:56:41 -0500

    Adding groups works as desired

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,10 +6,24 @@ class GroupsController < ApplicationController
     @membership = Membership.new
   end
 
+  # POST artists/:artist_id/groups
+  def create
+    @membership = Membership.new(member_params)
+    if @membership.save
+      flash[:success] = "Group added!"
+      redirect_to @member
+    else
+      render 'new' 
+    end
+  end
+
   private
 
     def set_member
       @member = Artist.find_by(id: params[:artist_id])
     end
 
+    def member_params
+      params.require(:membership).permit(:band_id, :member_id)
+    end
 end
