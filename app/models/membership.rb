b77253778ba9cc8432990e2d92ded38b52bac1b7
Matt Parsons <parsonsmatt@gmail.com>
2014-12-31 18:25:15 -0500

    Cleaned up validations

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,22 +2,23 @@ class Membership < ActiveRecord::Base
   belongs_to :band, class_name: "Artist"
   belongs_to :member, class_name: "Artist"
 
-  validates :band, presence: true
-  validates :member, presence: true
+  validates :band, presence: true, uniqueness: { scope: :member }
+  validates :member, presence: true, uniqueness: { scope: :band }
 
   validate :cannot_relate_to_self, :cannot_symmetric_relation
-  validates :band, uniqueness: { scope: :member }
-  validates :member, uniqueness: { scope: :band }
 
-  def cannot_relate_to_self
-    if band == member
-      errors.add(:band, " can't relate to self.")
-    end
-  end
+  private
 
-  def cannot_symmetric_relation
-    if Membership.where(band: member, member: band).any?
-      errors.add(:band, " can't relate symmetrically.")
+    def cannot_relate_to_self
+      if band == member
+        errors.add(:band, " can't relate to self.")
+      end
     end
-  end
+
+    def cannot_symmetric_relation
+      if Membership.where(band: member, member: band).any?
+        errors.add(:band, " can't relate symmetrically.")
+      end
+    end
+
 end
