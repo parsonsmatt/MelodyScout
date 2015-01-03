19bef79118076cd4e8f835aa21b8bed7564e444e
Matt Parsons <parsonsmatt@gmail.com>
2015-01-02 20:00:24 -0500

    Added login checks to artists controller

--- test/controllers/artists_controller_test.rb
+++ test/controllers/artists_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class ArtistsControllerTest < ActionController::TestCase
   setup do
     @artist = artists(:one)
+    @user = users(:one)
   end
 
   test "should get index" do
@@ -29,21 +30,39 @@ class ArtistsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should get edit if logged in" do
+    log_in_as(@user)
     get :edit, id: @artist
     assert_response :success
   end
 
-  test "should update artist" do
+  test "should not get edit if not logged in" do
+    get :edit, id: @artist
+    assert_redirected_to login_path
+  end
+
+  test "should update artist if logged in" do
+    log_in_as(@user)
     patch :update, id: @artist, artist: { description: @artist.description, name: @artist.name }
     assert_redirected_to artist_path(assigns(:artist))
   end
 
-  test "should destroy artist" do
+  test "should not update artist if not logged in" do
+    patch :update, id: @artist, artist: { description: @artist.description, name: @artist.name }
+    assert_redirected_to login_path
+  end
+
+  test "should destroy artist if admin" do
+    log_in_as(users(:admin))
     assert_difference('Artist.count', -1) do
       delete :destroy, id: @artist
     end
 
     assert_redirected_to artists_path
   end
+
+  test "should not destroy artist if not admin" do
+    delete :destroy, id: @artist
+    assert_redirected_to root_path
+  end  
 end
