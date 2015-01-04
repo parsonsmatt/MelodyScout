52c49a333083c01ac34010c6426918bb7fd5b957
Matt Parsons <parsonsmatt@gmail.com>
2015-01-03 23:22:56 -0500

    Remove date info from release test

--- test/models/release_test.rb
+++ test/models/release_test.rb
@@ -1,19 +1,10 @@
-require 'test_helper'
+ require 'test_helper'
 
 class ReleaseTest < ActiveSupport::TestCase
 
   def setup
-    @release = Release.create(name: "Test Release", description: "Lorem Ipsum", date: 1.year.ago)
+    @release = Release.new(name: "Test Release", description: "Lorem Ipsum")
   end
 
-  test "should have release date" do
-    @release.date = nil
-    assert_not @release.valid?
-  end
 
-  test "can add artist" do
-    artist = Artist.create(name: "test")
-    @release.add_artist(artist)
-    assert @release.artists.include?(artist)
-  end
 end
