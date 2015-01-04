711dd69f27b360e8d67bcea8a56d3dada85766d9
Matt Parsons <parsonsmatt@gmail.com>
2015-01-03 23:17:34 -0500

    Add release_dates to Release

--- app/models/release.rb
+++ app/models/release.rb
@@ -1,9 +1,10 @@
 class Release < ActiveRecord::Base
   validates :name, presence: true
-  validates :date, presence: true
 
   has_many :contributions
   has_many :artists, through: :contributions
+
+  has_many :release_dates
 
   def add_artist(artist)
     contributions.create(artist_id: artist.try(:id))
