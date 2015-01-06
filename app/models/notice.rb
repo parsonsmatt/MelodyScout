22766251a49960c12589ae60a95b2beec29cbefa
Matt Parsons <parsonsmatt@gmail.com>
2015-01-06 13:54:15 -0500

    Add validations

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,4 +1,7 @@
 class Notice < ActiveRecord::Base
   belongs_to :user
   belongs_to :notification
+
+  validates :notification_id, presence: true, uniqueness: { scope: :user_id }
+  validates :user_id, presence: true, uniqueness: { scope: :user_id }
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,10 @@
 class Notification < ActiveRecord::Base
   belongs_to :release
   belongs_to :release_date
-  belongs_to :notice
+  
+  has_many :notices
+  has_many :users, through: :notices
+  
+  validates :release_id, presence: true, uniqueness: { scope: :release_date_id }
+  validates :release_date_id, presence: true
 end
