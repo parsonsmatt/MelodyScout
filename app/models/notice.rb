364ddea0462b119b52e116ea977bbdc67b118839
Matt Parsons <parsonsmatt@gmail.com>
2015-01-07 00:09:24 -0500

    uhhh what

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -3,5 +3,5 @@ class Notice < ActiveRecord::Base
   belongs_to :notification
 
   validates :notification_id, presence: true, uniqueness: { scope: :user_id }
-  validates :user_id, presence: true, uniqueness: { scope: :user_id }
+  validates :user_id, presence: true, uniqueness: { scope: :notification_id }
 end
