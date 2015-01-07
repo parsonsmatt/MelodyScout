ce5b7e243e7a44535847be7bab737f685ff35ed4
Matt Parsons <parsonsmatt@gmail.com>
2015-01-07 00:28:05 -0500

    it shows the release_date id

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -2,6 +2,6 @@ class Notice < ActiveRecord::Base
   belongs_to :user
   belongs_to :notification
 
-  validates :notification_id, presence: true, uniqueness: { scope: :user_id }
+  validates :notification_id, presence: true
   validates :user_id, presence: true, uniqueness: { scope: :notification_id }
 end
