e91b3d3a6df2469a45294fdd9b7310460f32969b
Matt Parsons <parsonsmatt@gmail.com>
2015-01-07 22:04:02 -0500

    Remove test

--- test/controllers/notices_controller_test.rb
+++ test/controllers/notices_controller_test.rb
@@ -1,9 +1,5 @@
 require 'test_helper'
 
 class NoticesControllerTest < ActionController::TestCase
-  test "should not get destroy if not logged in" do
-    get :destroy
-    assert_redirected_to login_path
-  end
 
 end
