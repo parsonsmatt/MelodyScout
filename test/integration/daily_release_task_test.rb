5af0ea3d771e2807f6ae56589ba2ada78cce330a
Matt Parsons <parsonsmatt@gmail.com>
2015-01-09 21:38:58 -0500

    test works

--- test/integration/daily_release_task_test.rb
+++ test/integration/daily_release_task_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class DailyReleaseTaskTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @release = Release.create(name: "test release")
+    @artist = @release.artists.create(name: "test artist")
+    @user = users(:one)
+    @user.follow(@artist)
+  end
+
+  test "daily_release only hits todays ReleaseDates" do
+    for num in -5..5
+      @release.release_dates.create(date: num.days.from_now)
+    end
+
+    assert_difference '@user.notices.count', 1 do
+      ReleaseDate.daily_release
+    end
+  end
 end
