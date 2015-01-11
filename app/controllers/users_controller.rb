9061b0067d8db6784d63b2b798361d118430ffea
Matt Parsons <parsonsmatt@gmail.com>
2015-01-11 16:07:59 -0500

    grep -rl 'TellMe' | xargs sed -i 's/TellMe/MelodyScout/g'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
     @user = User.new(secure_params)
     if @user.save
       @user.send_activation_email
-      flash[:success] = "Welcome to TellMe!"
+      flash[:success] = "Welcome to MelodyScout!"
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
