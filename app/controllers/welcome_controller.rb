5997bf6c71ade64a3151bd0b44f12fe9584fa57b
Matt Parsons <parsonsmatt@gmail.com>
2015-01-10 23:26:27 -0500

    Add update stream to index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,11 @@
 class WelcomeController < ApplicationController
   def index
+    @updates = []
+    @updates += Release.order(updated_at: :desc).take(5)
+    @updates += Artist.order(updated_at: :desc).take(5)
+    @updates = @updates.sort_by { |a| a.updated_at }.
+                        reverse.
+                        map { |a| {name: a.name, id: a.id, klass: a.class} }
   end
 
   def about
