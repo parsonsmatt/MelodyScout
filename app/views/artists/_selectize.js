6b6d9434b020f7cc756daa1be78151a41794a05b
Matt Parsons <parsonsmatt@gmail.com>
2015-01-12 23:03:52 -0500

    move script into js file

---
+++ app/views/artists/_selectize.js
@@ -0,0 +1,25 @@
+$('document').ready(function() {
+	var seConfig = {
+		valueField: 'id',
+		labelField: 'name',
+		create: function(input) {
+			return {
+				'id': 0,
+				'name': input
+			}
+		},
+		createOnBlur: true,
+		searchField: 'name',  
+		delimiter: ''  
+	};
+
+
+	$('.select-release').selectize(seConfig);
+
+	$('.add_fields').click(function() { 
+		setTimeout(function() {
+			console.log('hey');
+			$('select.select-release').not('.selectized').selectize(seConfig);
+		}, 1); 
+	});
+});
