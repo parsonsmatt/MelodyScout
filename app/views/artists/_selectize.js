70b4122485d487e6ea1c3145af27989cab85364a
Matt Parsons <parsonsmatt@gmail.com>
2015-01-12 23:10:19 -0500

    finally fixed js formatting

--- app/views/artists/_selectize.js
+++ app/views/artists/_selectize.js
@@ -1,25 +1,24 @@
 $('document').ready(function() {
-	var seConfig = {
-		valueField: 'id',
-		labelField: 'name',
-		create: function(input) {
-			return {
-				'id': 0,
-				'name': input
-			}
-		},
-		createOnBlur: true,
-		searchField: 'name',  
-		delimiter: ''  
-	};
+    var seConfig = {
+        valueField: 'id',
+        labelField: 'name',
+        create: function(input) {
+            return {
+                'id': 0,
+                'name': input
+            }
+        },
+        createOnBlur: true,
+        searchField: 'name',  
+        delimiter: ''  
+    };
 
+    $('.select-release').selectize(seConfig);
 
-	$('.select-release').selectize(seConfig);
-
-	$('.add_fields').click(function() { 
-		setTimeout(function() {
-			console.log('hey');
-			$('select.select-release').not('.selectized').selectize(seConfig);
-		}, 1); 
-	});
+    $('.add_fields').click(function() { 
+        setTimeout(function() {
+            console.log('hey');
+            $('select.select-release').not('.selectized').selectize(seConfig);
+        }, 1); 
+    });
 });
