71f3aafb70fdccf23458d06471d7310d7171f405
Matt Parsons <parsonsmatt@gmail.com>
2015-01-13 23:40:12 -0500

    begin configuring capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,11 @@
 # config valid only for current version of Capistrano
 lock '3.3.5'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'MelodyScout'
+set :repo_url, 'git@github.com:parsonsmatt/melodyscout.git'
 
+set :user, "matt"
+set :use_sudo, false
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
