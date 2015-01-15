71df6734963910131f6a287a4b382b9ff7eb2389
Matt Parsons <parsonsmatt@gmail.com>
2015-01-15 15:11:53 -0500

    change dirname

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.3.5'
 
-set :application, 'MelodyScout'
+set :application, 'melodyscout'
 set :repo_url, 'git@github.com:melodyscout/melodyscout.git'
 
 set :user, "melodyscout"
