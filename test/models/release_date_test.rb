ebccf19a39727c7ded1e2c6de3570a594c134b98
Matt Parsons <parsonsmatt@gmail.com>
2015-01-17 00:55:40 -0500

    Clean up model tests

--- test/models/release_date_test.rb
+++ test/models/release_date_test.rb
@@ -1,7 +1,4 @@
 require 'test_helper'
 
 class ReleaseDateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 end
--- test/models/release_test.rb
+++ test/models/release_test.rb
@@ -2,37 +2,4 @@
 
 class ReleaseTest < ActiveSupport::TestCase
 
-  def setup
-    @release = Release.create(name: "Test Release", description: "Lorem Ipsum")
-  end
-
-  test "should add artist object" do
-    artist = Artist.create(name: "test artist")
-    assert_difference '@release.artists.count', 1 do
-      @release.add_artist(artist)
-    end
-    assert_includes @release.artists, artist
-  end
-
-  test "should add artist string" do
-    name = "test artist"
-    assert_equal String, name.class
-    artist = Artist.create(name: name)
-    assert_difference '@release.artists.count', 1 do
-      @release.add_artist name
-    end
-    assert_includes @release.artists, artist
-  end
-
-  test "should fail when artist not there" do
-    assert_no_difference '@release.artists.count' do
-      @release.add_artist "i am not in the db"
-    end
-  end
-
-  test "should raise argument error if neither artist nor string" do
-    assert_raise ArgumentError do
-      @release.add_artist 234
-    end
-  end
 end
