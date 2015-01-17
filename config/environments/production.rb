2e8395a6def22747183c2de60df7f2a6b59f8a96
Matt Parsons <parsonsmatt@gmail.com>
2015-01-16 21:23:08 -0500

    Fix host configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,13 +80,14 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'melodyscout.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options[:host] = host
   ActionMailer::Base.smtp_settings = {
     address:                'smtp.gmail.com',
     port:                   '587',
     authentication:         :plain,
     user_name:              ENV['GMAIL_USERNAME'],
     password:               ENV['GMAIL_PASSWORD'],
-    enable_starttles_auto:  true
+    enable_starttles_auto:  true,
+    domain:                 'melodyscout.herokuapp.com'
   }
 end
