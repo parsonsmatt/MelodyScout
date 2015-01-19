28b75740c7471d7643124736246c9846faaf6ec3
Matt Parsons <parsonsmatt@gmail.com>
2015-01-19 13:19:51 -0500

    configured

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,8 +6,77 @@ set :repo_url,      'git@github.com:MelodyScout/MelodyScout'
 set :user,          'melodyscout'
 set :puma_threads,  [4,16]
 set :puma_workers,  0
+
 # TODO: set to master after done, while I'm testing...
 set :branch,        'digitalocean' 
+set :stage,         :production
+set :deploy_via,    :remote_cache
+set :copy_exclude,  [ '.git' ]
+
+# From coderwall/p/ttrhow/deploying-raisl-app-using-nginx-puma-and-capistrano-3 :
+
+set :puma_bind,     "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}}-puma.sock"
+set :puma_pid,      "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,"#{release_path}/log/puma.access.log"
+set :ssh_options,   {
+  forward_agent:    true,
+  user:             fetch(:user),
+  keys:             %w(~/.ssh_id_rsa.pub) 
+}
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true
+
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
+
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
+  # TODO: Uncomment when going to master
+  # before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
+end
+
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, '/var/www/my_app_name'
@@ -32,16 +101,3 @@ set :branch,        'digitalocean'
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
-
-namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-end
