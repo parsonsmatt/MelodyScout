7cb8538de6328c0848ae6206aa81460232159f24
Matt Parsons <parsonsmatt@gmail.com>
2015-01-19 20:14:57 -0500

    Filter works crudely

--- app/assets/javascripts/index_filter.js
+++ app/assets/javascripts/index_filter.js
@@ -1,5 +1,19 @@
-$(function() {
-    $('#artist-filter-input').change(function() {
-        $('#artist-filter-text').text( $('#artist-filter-input').text() );
+var filterTables = function() {
+    $('#filter-input').keyup(function() {
+        var filterString = $('#filter-input').val().toUpperCase();
+
+        $.each($('.filter'), function(i, row) {
+            if ( $(row).html().toUpperCase().indexOf(filterString) > -1 ) {
+                $(row).show();
+            }
+            else {
+                $(row).hide();
+            }
+        });
     });
-});
+};
+
+$(document).ready(filterTables);
+$(document).on('page:load', filterTables);
+        
+        
