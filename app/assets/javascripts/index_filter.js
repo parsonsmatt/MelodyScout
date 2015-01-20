c563712904d80e28d85a3e9fbb69cbac275869dc
Matt Parsons <parsonsmatt@gmail.com>
2015-01-19 22:30:17 -0500

    filter selects regardless of whitespace or special characters

--- app/assets/javascripts/index_filter.js
+++ app/assets/javascripts/index_filter.js
@@ -2,9 +2,10 @@ var MelodyScout = MelodyScout || {};
 
 MelodyScout.filterTables = function() {
     $('#filter-input').keyup(function() {
-        var filterString = $('#filter-input').val().toUpperCase();
+        var filterString = $('#filter-input').val().toUpperCase().replace(/\W+/g, '');
+        console.log(filterString);
         $.each($('.filter'), function(i, row) {
-            if ( $(row).html().toUpperCase().indexOf(filterString) > -1 ) {
+            if ( $(row).html().toUpperCase().replace(/\W+/g, '').indexOf(filterString) > -1 ) {
                 $(row).show();
             }
             else {
