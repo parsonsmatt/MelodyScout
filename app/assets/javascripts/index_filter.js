6f44233fc2c04addaf683876d1520449dfacb65d
Matt Parsons <parsonsmatt@gmail.com>
2015-01-19 20:21:50 -0500

    filter tables hooray

--- app/assets/javascripts/index_filter.js
+++ app/assets/javascripts/index_filter.js
@@ -1,7 +1,6 @@
 var filterTables = function() {
     $('#filter-input').keyup(function() {
         var filterString = $('#filter-input').val().toUpperCase();
-
         $.each($('.filter'), function(i, row) {
             if ( $(row).html().toUpperCase().indexOf(filterString) > -1 ) {
                 $(row).show();
